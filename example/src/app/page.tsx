00e15168f1e2bb992f9a7cf56f9e52729d09c0d0
Sal Samani <sal@milysec.com>
2024-08-27 13:51:28 +1000

    logo is now a link

--- example/src/app/page.tsx
+++ example/src/app/page.tsx
@@ -5,7 +5,7 @@ import { useWallet } from "@solana/wallet-adapter-react";
 import { ConnectButton } from "@/components/buttons/ConnectButton";
 import { StartButton } from "@/components/buttons/StartButton";
 import { SubdomainSearch } from "@/components/SubdomainSearch";
-
+import Link from "next/link";
 export default function Home() {
   const { connected } = useWallet();
 
@@ -15,14 +15,16 @@ export default function Home() {
         <ConnectButton />
       </div>
       <div className="m-auto flex max-w-[90vw] grow flex-col items-center justify-center pb-16">
-        <Image
-          className="relative mb-6 size-20"
-          src="/images/sns.svg"
-          alt="MyBlink Logo"
-          width={33}
-          height={38}
-          priority
-        />
+        <Link href={'/'}>
+          <Image
+            className="relative mb-6 size-20"
+            src="/images/sns.svg"
+            alt="MyBlink Logo"
+            width={33}
+            height={38}
+            priority
+          />
+        </Link>
         <h1 className="mb-4 text-center text-2xl">
           MyBlink.sol
         </h1>
